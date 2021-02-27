178eba4dc51b452c2c374a88555b6b767a3c0dce
webliam <67931798+webliam@users.noreply.github.com>
2021-02-27 10:28:40 -0500

    Add files via upload
    welcome screen for new accounting software your company is developing

---
+++ AccountingSystem.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Acme_Accounting_Systems
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, We're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
