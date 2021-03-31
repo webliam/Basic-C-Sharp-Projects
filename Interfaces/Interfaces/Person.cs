e49002f159a8a01c0b390b5eb1e67d6393bb5a39
webliam <webliam@yahoo.com>
2021-03-31 14:36:56 -0400

    Interfaces Assignment
    Create an interface called IQuittable and have it define a void method called Quit().
    Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
    Use polymorphism to create an object of type IQuittable and call the Quit() method on it. Hint: an object can be of an interface type if it implements that specific interface.

---
+++ Interfaces/Interfaces/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+            Console.ReadLine();
+        }
+
+    }
+}
