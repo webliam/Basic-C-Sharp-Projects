6066159e357664030348f0d99c8baa4a551ad503
webliam <webliam@yahoo.com>
2021-03-31 22:11:19 -0400

    initial commit
    Create an interface called IQuittable and have it define a void method called Quit().
    Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
    Use polymorphism to create an object of type IQuittable and call the Quit() method on it. Hint: an object can be of an interface type if it implements that specific interface.

---
+++ Interface/Interfaces/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            string fullName = FirstName + " " + LastName;
+            Console.WriteLine(fullName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("To exit, press enter.");
+            Console.ReadLine();
+        }
+    }
+}
