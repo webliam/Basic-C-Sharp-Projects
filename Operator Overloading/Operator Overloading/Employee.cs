2294cb6e04bd28b9ad12bd320f57bbc5a9e77456
webliam <webliam@yahoo.com>
2021-03-31 20:17:51 -0400

    New

---
+++ Operator Overloading/Operator Overloading/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    public class Employee : Person
+    {
+        public static bool operator ==(Employee employee, Employee employee1)
+        {
+            if (employee.ID == employee1.ID)
+            {
+                bool isEqual = true;
+                return isEqual;
+            }
+            else
+            {
+                bool isEqual = false;
+                return isEqual;
+            }
+        }
+        public static bool operator !=(Employee employee, Employee employee1)
+        {
+            if (employee.ID != employee1.ID)
+            {
+                bool isNotEqual = true;
+                return isNotEqual;
+            }
+            else
+            {
+                bool isNotEqual = false;
+                return isNotEqual;
+            }
+
+        }
+
+
+    }
+}
---
+++ Operator Overloading/Operator Overloading/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName + " ID: " + ID);
+            Console.ReadLine();
+        }
+
+    }
+}
