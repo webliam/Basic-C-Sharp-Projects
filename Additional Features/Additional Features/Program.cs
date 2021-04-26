608438c3eb6fca6dc7494c2611faf5b2335ee228
webliam <webliam@yahoo.com>
2021-04-25 23:28:29 -0400

    update

--- Additional Features/Additional Features/Program.cs
+++ Additional Features/Additional Features/Program.cs
@@ -10,6 +10,12 @@ namespace Additional_Features
     {
         static void Main(string[] args)
         {
+            const int currentYear = 2021;
+            Console.WriteLine("The current year is {0}", currentYear);
+
+            var previousYear = new Time("previous Year", 2020);
+            Console.WriteLine("The previous year is {0}", previousYear.Year);
+            Console.ReadLine();
         }
     }
 }
