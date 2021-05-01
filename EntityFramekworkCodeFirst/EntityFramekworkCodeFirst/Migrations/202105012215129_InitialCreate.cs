c827895b906a7e4fe35ab67ebc3c26f807f2b84f
webliam <webliam@yahoo.com>
2021-05-01 19:05:45 -0400

    new

---
+++ EntityFramekworkCodeFirst/EntityFramekworkCodeFirst/Migrations/202105012215129_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿namespace EntityFramekworkCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Grades",
+                c => new
+                    {
+                        GradeId = c.Int(nullable: false, identity: true),
+                        GradeName = c.String(),
+                        Section = c.String(),
+                    })
+                .PrimaryKey(t => t.GradeId);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentID = c.Int(nullable: false, identity: true),
+                        StudentName = c.String(),
+                        DateOfBirth = c.DateTime(),
+                        Photo = c.Binary(),
+                        Height = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Weight = c.Single(nullable: false),
+                        Grade_GradeId = c.Int(),
+                    })
+                .PrimaryKey(t => t.StudentID)
+                .ForeignKey("dbo.Grades", t => t.Grade_GradeId)
+                .Index(t => t.Grade_GradeId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Students", "Grade_GradeId", "dbo.Grades");
+            DropIndex("dbo.Students", new[] { "Grade_GradeId" });
+            DropTable("dbo.Students");
+            DropTable("dbo.Grades");
+        }
+    }
+}
---
+++ EntityFramekworkCodeFirst/EntityFramekworkCodeFirst/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace EntityFramekworkCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EntityFramekworkCodeFirst.SchoolContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "EntityFramekworkCodeFirst.SchoolContext";
+        }
+
+        protected override void Seed(EntityFramekworkCodeFirst.SchoolContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
