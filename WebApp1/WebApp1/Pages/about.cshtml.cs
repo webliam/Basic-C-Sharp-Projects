83907e313242fc0d097e6cc89635637587f54e55
webliam <webliam@yahoo.com>
2021-05-01 12:48:16 -0400

    update

---
+++ WebApp1/WebApp1/Pages/about.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp1.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+        }
+        public void Onpost()
+        {
+            Message = "Your application1 description page.";
+        }
+    }
+}
